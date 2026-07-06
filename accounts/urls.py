375918df6f7bdea64e010c7d51ba6d82e52216df
Harsh <harshbhatiya120@gmail.com>
2026-07-06 10:42:27 +0530

    Delete accounts directory

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from accounts import views
-
-urlpatterns =[
-    path('signupaccount/',views.signupaccount,name='signupaccount'),
-    path('logout/',views.logoutaccount,name='logoutaccount'),
-    path('login/',views.loginaccount,name='loginaccount'),
-
-]
