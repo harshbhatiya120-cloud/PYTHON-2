60f89c56ec7deb2536c2dfac8560a4b6b2352336
Harsh <harshbhatiya120@gmail.com>
2026-07-06 10:43:30 +0530

    Delete news directory

--- news/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from news import views
-
-urlpatterns = [
-    path('',views.news,name='news')
-]
--- news/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import News
-# Create your views here.
-def news(request):
-    newss = News.objects.all()
-    return render(request,'news.html',{'newss':newss})
